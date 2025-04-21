60e0cd2b087cb1cf63f65ef1255feea11efee8bd
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-04-21 16:09:51 +0530

    completed

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv')
 dotenv.config();
 const connectionToDB = await mongoose.connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 30000, 
-    connectTimeoutMS: 30000,
-    socketTimeoutMS: 45000,
-  });
+  serverSelectionTimeoutMS: 30000, // Increase to 30 seconds
+  connectTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
+});
 module.exports = connectionToDB;
