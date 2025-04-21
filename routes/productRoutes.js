aedea8263097970ceac3ab9ab54b7fd9105fdbed
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-04-21 14:47:09 +0530

    completed

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,9 +4,9 @@ const ProductRouter = express.Router();
 
 
 ProductRouter.post('/create', createProductData);
-ProductRouter.get('/products', getProductData);
-ProductRouter.put('/products/:id', updateProductData);
-ProductRouter.delete('/products/:id', deleteProductData);
-ProductRouter.get('/products/:id', getSingleData);
+ProductRouter.get('/get', getProductData);
+ProductRouter.put('/update/:id', updateProductData);
+ProductRouter.delete('/delete/:id', deleteProductData);
+ProductRouter.get('/get/:id', getSingleData);
 
 module.exports = ProductRouter;
